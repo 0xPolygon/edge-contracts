a18d817591b31a0a0607ae15510b72cd810fbdfb
qedk <1994constant@gmail.com>
2022-04-28 15:28:40 +0530

    chore: Fix lint

--- contracts/ethereum/EthereumValidatorSet.sol
+++ contracts/ethereum/EthereumValidatorSet.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 
 interface IBLS {
-	function verifySingle(
+    function verifySingle(
         uint256[2] calldata signature,
         uint256[4] calldata pubkey,
         uint256[2] calldata message
@@ -14,40 +14,44 @@ interface IBLS {
 }
 
 contract EthereumValidatorSet is Initializable, Ownable {
-	using EnumerableSet for EnumerableSet.AddressSet;
+    using EnumerableSet for EnumerableSet.AddressSet;
 
-	struct Validator {
-		uint256 id;
-		address ethAddress;
-		uint256[4] blsKey;
-	}
+    struct Validator {
+        uint256 id;
+        address ethAddress;
+        uint256[4] blsKey;
+    }
 
-	uint256 currentValidatorId;
-	uint256[2] message;
-	mapping(uint256 => Validator) validators;
-	EnumerableSet.AddressSet whitelist;
+    uint256 public currentValidatorId;
+    uint256[2] public message;
+    mapping(uint256 => Validator) public validators;
+    EnumerableSet.AddressSet private whitelist;
 
-	modifier isWhitelisted(address _address) {
-		require(whitelist.contains(_address), "NOT_WHITELISTED");
+    modifier isWhitelisted(address _address) {
+        require(whitelist.contains(_address), "NOT_WHITELISTED");
 
-		_;
-	}
+        _;
+    }
 
-	function initialize(Validator[] calldata _validators, uint256[2] calldata _message) external initializer {
-		for (uint256 i = 0; i < _validators.length; i++) {
-			validators[currentValidatorId++] = _validators[i];
-		}
-		message = _message;
-		_transferOwnership(msg.sender);
- 	}
+    function initialize(
+        Validator[] calldata _validators,
+        uint256[2] calldata _message
+    ) external initializer {
+        for (uint256 i = 0; i < _validators.length; i++) {
+            validators[currentValidatorId++] = _validators[i];
+        }
+        message = _message;
+        _transferOwnership(msg.sender);
+    }
 
- 	function addToWhitelist(address[] calldata _whitelisted) external {
- 		for (uint256 i = 0; i < _whitelisted.length; i++) {
- 			whitelist.add(_whitelisted[i]);
- 		}
- 	}
+    function addToWhitelist(address[] calldata _whitelisted) external {
+        for (uint256 i = 0; i < _whitelisted.length; i++) {
+            whitelist.add(_whitelisted[i]);
+        }
+    }
 
- 	function register(uint256[2] calldata signature, uint256[4] calldata pubkey) external isWhitelisted(msg.sender) {
-
- 	}
- }
+    function register(uint256[2] calldata signature, uint256[4] calldata pubkey)
+        external
+        isWhitelisted(msg.sender)
+    {}
+}
