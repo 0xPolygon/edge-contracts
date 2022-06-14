016e2a7d75047aad4b30c88fa6810ff2db32afa5
zorancuc <cuc.zoran@gmail.com>
2022-06-14 05:58:26 +0200

    implement rootvalidator initialize & not enough signature branch

--- test/root/CheckpointManager.test.ts
+++ test/root/CheckpointManager.test.ts
@@ -13,6 +13,10 @@ import {
 
 const DOMAIN = ethers.utils.hexlify(ethers.utils.randomBytes(32));
 
+const DOMAIN_ROOTVALIDATOR = ethers.utils.arrayify(
+  ethers.utils.hexlify(ethers.utils.randomBytes(32))
+);
+
 describe("CheckpointManager", () => {
   let bls: BLS,
     bn256G2: BN256G2,
@@ -58,7 +62,74 @@ describe("CheckpointManager", () => {
     expect(await checkpointManager.rootValidatorSet()).to.equal(
       rootValidatorSet.address
     );
+    expect(await rootValidatorSet.activeValidatorSetSize()).to.equal(0);
     expect(await checkpointManager.domain()).to.equal(DOMAIN);
+  });
+
+  it("Submit with invalid length", async () => {
+    submitCounter = 1;
+    const checkPoint = {
+      startBlock: 1,
+      endBlock: 100,
+      eventRoot: ethers.utils.hexlify(ethers.utils.randomBytes(32)),
+    };
+
+    await expect(
+      checkpointManager.submit(
+        submitCounter,
+        checkPoint,
+        [
+          ethers.utils.hexlify(ethers.utils.randomBytes(32)),
+          ethers.utils.hexlify(ethers.utils.randomBytes(32)),
+        ],
+        []
+      )
+    ).to.be.revertedWith("NOT_ENOUGH_SIGNATURES");
+  });
+
+  it("Initialize RootValidator and validate initialization", async () => {
+    const messagePoint = mcl.g1ToHex(
+      mcl.hashToPoint(
+        ethers.utils.hexlify(ethers.utils.toUtf8Bytes("polygon-v3-validator")),
+        DOMAIN_ROOTVALIDATOR
+      )
+    );
+    // validatorSetSize = Math.floor(Math.random() * (5 - 1) + 4); // Randomly pick 1-5
+    validatorSetSize = 3;
+    let addresses = [];
+    let pubkeys = [];
+    for (let i = 0; i < validatorSetSize; i++) {
+      const { pubkey, secret } = mcl.newKeyPair();
+      pubkeys.push(mcl.g2ToHex(pubkey));
+      addresses.push(accounts[i].address);
+    }
+    await rootValidatorSet.initialize(
+      bls.address,
+      addresses,
+      pubkeys,
+      messagePoint
+    );
+
+    // console.log(await rootValidatorSet.validators(2));
+
+    expect(await rootValidatorSet.currentValidatorId()).to.equal(
+      validatorSetSize
+    );
+    expect(ethers.utils.hexValue(await rootValidatorSet.message(0))).to.equal(
+      ethers.utils.hexValue(messagePoint[0])
+    );
+    expect(ethers.utils.hexValue(await rootValidatorSet.message(1))).to.equal(
+      ethers.utils.hexValue(messagePoint[1])
+    );
+    for (let i = 0; i < validatorSetSize; i++) {
+      const validator = await rootValidatorSet.validators(i + 1);
+      expect(validator.id).to.equal(i + 1);
+      expect(validator._address).to.equal(addresses[i]);
+      expect(
+        await rootValidatorSet.validatorIdByAddress(addresses[i])
+      ).to.equal(i + 1);
+      // expect(validator.blsKey).to.equal(pubkeys[i]); //typings for this aren't generated...
+    }
   });
 
   it("Submit with non-sequential id", async () => {
@@ -69,8 +140,10 @@ describe("CheckpointManager", () => {
       eventRoot: ethers.utils.hexlify(ethers.utils.randomBytes(32)),
     };
 
+    console.log("validator size")
+    console.log(await rootValidatorSet.activeValidatorSetSize());
     await expect(
-      checkpointManager.submit(submitCounter, checkPoint, [DOMAIN, DOMAIN], [])
+      checkpointManager.submit(submitCounter, checkPoint, [DOMAIN, DOMAIN], [1, 2, 3])
     ).to.be.revertedWith("ID_NOT_SEQUENTIAL");
   });
 
@@ -114,27 +187,6 @@ describe("CheckpointManager", () => {
         []
       )
     ).to.be.revertedWith("EMPTY_CHECKPOINT");
-  });
-
-  it("Submit with invalid length", async () => {
-    submitCounter = 1;
-    const checkPoint = {
-      startBlock: 1,
-      endBlock: 100,
-      eventRoot: ethers.utils.hexlify(ethers.utils.randomBytes(32)),
-    };
-
-    await expect(
-      checkpointManager.submit(
-        submitCounter,
-        checkPoint,
-        [
-          ethers.utils.hexlify(ethers.utils.randomBytes(32)),
-          ethers.utils.hexlify(ethers.utils.randomBytes(32)),
-        ],
-        []
-      )
-    ).to.be.revertedWith("NOT_ENOUGH_SIGNATURES");
   });
 
   it("Submit", async () => {
