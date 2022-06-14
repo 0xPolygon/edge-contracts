b9e6d9744637d063e6cf08c2bcec468c011de5ea
Jaynti Kanani <jdkanani@gmail.com>
2022-06-14 13:58:14 +0400

    Add state receiver (#6)
    * fix: refactor
    
    * new: add state receiver draft
    
    * fix: counter vs id
    
    * fix: use uint256
    
    * new: add sig verification
    
    * fix: use counter instead of id
    
    * fix: reentrancy
    
    * fix: use obj
    
    * new: add state sync batch
    
    * fix: bug for batch
    
    * feat: Remove SLOADs, some style fixes, skip EOA calls
    
    * fix: Fix counter for single state sync, minor optimizations
    
    * fix: Fix slither, make variable names concise, adjust gas costs, internalize some logic
    
    * fix: Adjust some low-level calls, add mocks
    
    * test: Add tests for contract
    
    * test: Add tests for state sync batch and empty batches
    
    * fix: Remove SLOADs from event
    
    * test: Add more branch tests
    
    * test: Add tests for all event types
    
    * fix: Optimize state sync skips
    
    * fix: Optimize always false bytecode
    
    * fix: Optimize test run times
    
    * lint: Fix solhint
    
    * implement untested branches of StateReceiver and StateSender
    
    implement untested branches of StateReceiver and StateSender
    
    * correct description and naming
    
    Co-authored-by: qedk <1994constant@gmail.com>
    Co-authored-by: zorancuc <cuc.zoran@gmail.com>

---
+++ contracts/mocks/StateReceivingContract.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract StateReceivingContract {
+    uint256 public counter;
+
+    function onStateReceive(
+        uint256, /* id */
+        address, /* sender */
+        bytes calldata data
+    ) external returns (bytes32) {
+        counter += abi.decode(data, (uint256));
+        return bytes32(counter);
+    }
+}
---
+++ test/constants.ts
@@ -0,0 +1,4 @@
+/// @notice This bytecode is used to mock and return true with any input
+export const alwaysTrueBytecode = "0x600160005260206000F3";
+/// @notice This bytecode is used to mock and return false with any input
+export const alwaysFalseBytecode = "0x60206000F3";
