3e29e7356ca6fc90594c3f7023212449744aebd5
zorancuc <cuc.zoran@gmail.com>
2022-07-12 15:30:09 +0200

    initialize StakeManager & ChildValidatorSte

--- test/child/StakeManager.test.ts
+++ test/child/StakeManager.test.ts
@@ -1,29 +1,29 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
-import { BigNumberish } from "ethers";
+import { BigNumber } from "ethers";
 import * as mcl from "../../ts/mcl";
+import * as hre from "hardhat";
 
 import { BLS, ChildValidatorSet, StakeManager } from "../../typechain";
 
 const DOMAIN = ethers.utils.hexlify(ethers.utils.randomBytes(32));
 
 describe("StakeManager", () => {
-  let bls: BLS,
+  let rootValidatorSetAddress: string,
     childValidatorSet: ChildValidatorSet,
+    systemChildValidatorSet: ChildValidatorSet,
     stakeManager: StakeManager,
-    newEpochReward: number,
-    newMinSelfStake: number,
-    newMinDelegation: number,
-    eventRoot: any,
-    validatorSecretKeys: any[],
+    epochReward: number,
+    minSelfStake: number,
+    minDelegation: number,
+    validatorSetSize: number,
+    validatorStake: BigNumber,
     accounts: any[]; // we use any so we can access address directly from object
   before(async () => {
     await mcl.init();
     accounts = await ethers.getSigners();
 
-    const BLS = await ethers.getContractFactory("BLS");
-    bls = await BLS.deploy();
-    await bls.deployed();
+    rootValidatorSetAddress = ethers.Wallet.createRandom().address;
 
     const ChildValidatorSet = await ethers.getContractFactory(
       "ChildValidatorSet"
@@ -35,15 +35,83 @@ describe("StakeManager", () => {
     stakeManager = await StakeManager.deploy();
     await stakeManager.deployed();
 
-    eventRoot = ethers.utils.randomBytes(32);
+    epochReward = 1;
+    minSelfStake = 10000;
+    minDelegation = 10000;
+
+    await hre.network.provider.send("hardhat_setBalance", [
+      "0xffffFFFfFFffffffffffffffFfFFFfffFFFfFFfE",
+      "0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF",
+    ]);
+    await hre.network.provider.request({
+      method: "hardhat_impersonateAccount",
+      params: ["0xffffFFFfFFffffffffffffffFfFFFfffFFFfFFfE"],
+    });
+
+    const systemSigner = await ethers.getSigner(
+      "0xffffFFFfFFffffffffffffffFfFFFfffFFFfFFfE"
+    );
+    systemChildValidatorSet = childValidatorSet.connect(systemSigner);
   });
 
   it("Initialize and validate initialization", async () => {
     await stakeManager.initialize(
-      newEpochReward,
-      newMinSelfStake,
-      newMinDelegation,
+      epochReward,
+      minSelfStake,
+      minDelegation,
       childValidatorSet.address
     );
+
+    expect(await stakeManager.epochReward()).to.equal(epochReward);
+    expect(await stakeManager.minSelfStake()).to.equal(minSelfStake);
+    expect(await stakeManager.minDelegation()).to.equal(minDelegation);
+
+    expect(await stakeManager.childValidatorSet()).to.equal(
+      childValidatorSet.address
+    );
+  });
+
+  it("Initialize ChildValidatorSet and validate initialization", async () => {
+    validatorSetSize = Math.floor(Math.random() * (5 - 1) + 1); // Randomly pick 1-5
+    validatorStake = ethers.utils.parseEther(
+      String(Math.floor(Math.random() * (10000 - 1000) + 1000))
+    );
+    const validatorStakes = Array(validatorSetSize).fill(validatorStake);
+    const addresses = [];
+    const pubkeys = [];
+    const validatorSet = [];
+    for (let i = 0; i < validatorSetSize; i++) {
+      const { pubkey, secret } = mcl.newKeyPair();
+      pubkeys.push(mcl.g2ToHex(pubkey));
+      addresses.push(accounts[i].address);
+      validatorSet.push(i + 1);
+    }
+    await systemChildValidatorSet.initialize(
+      rootValidatorSetAddress,
+      stakeManager.address,
+      addresses,
+      pubkeys,
+      validatorStakes,
+      validatorSet
+    );
+    expect(await childValidatorSet.currentValidatorId()).to.equal(
+      validatorSetSize
+    );
+    expect(await childValidatorSet.stakeManager()).to.equal(
+      stakeManager.address
+    );
+    for (let i = 0; i < validatorSetSize; i++) {
+      const validator = await childValidatorSet.validators(i + 1);
+      expect(validator._address).to.equal(addresses[i]);
+      expect(validator.selfStake).to.equal(validatorStake);
+      expect(validator.totalStake).to.equal(validatorStake);
+      expect(
+        await childValidatorSet.validatorIdByAddress(addresses[i])
+      ).to.equal(i + 1);
+    }
+  });
+
+  it("SelfStake less amount than minSelfStake", async () => {
+    
   });
 });
