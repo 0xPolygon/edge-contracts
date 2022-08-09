94e5ca52a7ea3c00aa248a519f72bf4ca5ddee42
ZeroEkkusu <94782988+ZeroEkkusu@users.noreply.github.com>
2022-08-09 14:57:32 +0200

    fix: use `onlyInitializing` in `Owned`

--- contracts/common/Owned.sol
+++ contracts/common/Owned.sol
@@ -9,7 +9,7 @@ abstract contract Owned is IOwned, Initializable {
     address public proposedOwner;
 
     /// @dev initializes the contract setting the deployer as the initial owner
-    function __Owned_init() internal initializer {
+    function __Owned_init() internal onlyInitializing {
         owner = msg.sender;
         emit OwnershipTransferred(address(0), msg.sender);
     }
