db38e1f9ccd3f35dc6569ca7d1744a60be395e3f
Zero Ekkusu <94782988+ZeroEkkusu@users.noreply.github.com>
2022-08-20 12:01:59 +0200

    test(Merkle): use Murky instead of merkletreejs Speed up fuzz tests significantly

--- test/forge/common/Merkle.t.sol
+++ test/forge/common/Merkle.t.sol
@@ -4,9 +4,15 @@ pragma solidity ^0.8.13;
 import {Merkle} from "contracts/common/Merkle.sol";
 
 import "../utils/TestPlus.sol";
+import {MurkyBase} from "murky/common/MurkyBase.sol";
 
-contract MerkleTest is TestPlus {
+contract MerkleTest is TestPlus, MurkyBase {
     using Merkle for bytes32;
+
+    /// @notice Hashing function for Murky
+    function hashLeafPairs(bytes32 left, bytes32 right) public pure override returns (bytes32 _hash) {
+        _hash = keccak256(abi.encode(left, right));
+    }
 
     function testCannotCheckMemership_InvalidIndex(uint256 index, uint8 proofSize) public {
         index = bound(index, 2**proofSize, type(uint256).max);
@@ -16,26 +22,19 @@ contract MerkleTest is TestPlus {
         this.checkMembershipHelper("", index, "", proof);
     }
 
-    function testCheckMembership(
-        bytes32[32] memory leaves,
-        uint256 index,
-        bytes32 randomHash
-    ) public {
-        // make hashes unique
-        for (uint256 i; i < leaves.length; ++i) {
-            leaves[i] = keccak256(abi.encode(leaves[i], i));
-        }
+    function testCheckMembership(bytes32[] memory leaves, uint256 index) public {
+        vm.assume(leaves.length > 1);
         // bound index
         index %= leaves.length - 1;
-        // get merkle root and proof from merkletreejs
-        string[] memory cmd = new string[](3);
-        cmd[0] = "node";
-        cmd[1] = "test/forge/utils/merkle-root-proof";
-        cmd[2] = vm.toString(abi.encode(leaves, index));
-        (bytes32 root, bytes32[] memory proof) = abi.decode(vm.ffi(cmd), (bytes32, bytes32[]));
+        // get merkle root and proof
+        bytes32 root = getRoot(leaves);
+        bytes32[] memory proof = getProof(leaves, index);
+        bytes32 leaf = leaves[index];
+        bytes32 randomDataHash = keccak256(abi.encode(leaf));
 
-        assertTrue(this.checkMembershipHelper(leaves[index], index, root, proof));
-        assertFalse(this.checkMembershipHelper(randomHash, index, root, proof));
+        // should return true for leaf and false for random hash
+        assertTrue(this.checkMembershipHelper(leaf, index, root, proof));
+        assertFalse(this.checkMembershipHelper(randomDataHash, index, root, proof));
     }
 
     /// @notice Helper for passing proof in calldata
@@ -44,7 +43,7 @@ contract MerkleTest is TestPlus {
         uint256 index,
         bytes32 rootHash,
         bytes32[] calldata proof
-    ) external returns (bool) {
+    ) external view returns (bool) {
         return leaf.checkMembership(index, rootHash, proof);
     }
 }
