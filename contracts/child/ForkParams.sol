0a6f1aec06c662643eae417bd4a79fee2dc0e50b
qedk <1994constant@gmail.com>
2022-12-23 15:48:19 +0530

    feat: add initial RFC-128 spec implementations

---
+++ contracts/child/ForkParams.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+    @title ForkParams
+    @author Polygon Technology (@QEDK)
+    @notice Configurable softfork features that are read by the client on each epoch
+    @dev The contract allows for configurable softfork parameters without genesis updation
+ */
+contract ForkParams is Ownable {
+    mapping(bytes32 => uint256) public featureToBlockNumber; // keccak256("FEATURE_NAME") -> blockNumber
+
+    /**
+     * @notice function to add a new feature at a block number
+     * @dev block number must be set in the future and feature must already not be scheduled
+     * @param blockNumber block number to schedule the feature
+     * @param feature feature name to schedule
+     */
+    function addNewFeature(uint256 blockNumber, string calldata feature) external onlyOwner {
+        require(blockNumber >= block.number, "ForkParams: INVALID_BLOCK");
+        bytes32 featureHash = keccak256(abi.encode(feature));
+        require(featureToBlockNumber[featureHash] == 0, "ForkParams: FEATURE_EXISTS");
+        featureToBlockNumber[featureHash] = blockNumber;
+    }
+
+    /**
+     * @notice function to update the block number for a feature
+     * @dev block number must be set in the future and feature must already be scheduled
+     * @param newBlockNumber new block number to schedule the feature at
+     * @param feature feature name to schedule
+     */
+    function updateFeatureBlock(uint256 newBlockNumber, string calldata feature) external onlyOwner {
+        require(newBlockNumber >= block.number, "ForkParams: INVALID_BLOCK");
+        bytes32 featureHash = keccak256(abi.encode(feature));
+        require(featureToBlockNumber[featureHash] != 0, "ForkParams: FEATURE_MUST_EXIST");
+        featureToBlockNumber[featureHash] = newBlockNumber;
+    }
+
+    /**
+     * @notice function to check if a feature is activated
+     * @dev returns true if feature is activated, false if feature is scheduled in the future and reverts if feature does not exists
+     * @param feature feature name to check for activation
+     */
+    function isFeatureActivated(string calldata feature) external view returns (bool) {
+        uint256 featureBlockNumber = featureToBlockNumber[keccak256(abi.encode(feature))];
+        require(featureBlockNumber != 0, "ForkParams: FEATURE_NOT_AVAILABLE");
+        if (block.number >= featureBlockNumber) {
+            return true;
+        }
+        return false;
+    }
+}
