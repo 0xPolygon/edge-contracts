327e6f19738b54732d94bae37f5592002c6e6c38
qedk <1994constant@gmail.com>
2023-01-23 04:15:13 +0400

    feat: add storage gap

--- contracts/child/ChildERC20.sol
+++ contracts/child/ChildERC20.sol
@@ -281,4 +281,11 @@ contract ChildERC20 is Context, EIP712MetaTransaction, IChildERC20 {
             }
         }
     }
+
+    /**
+     * @dev This empty reserved space is put in place to allow future versions to add new
+     * variables without shifting down storage in the inheritance chain.
+     * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
+     */
+    uint256[46] private __gap;
 }
