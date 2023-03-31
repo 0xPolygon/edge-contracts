2c97fd8f861dcbc3dc1fff790d00219eb372b6a5
gretzke <daniel@gretzke.de>
2023-03-31 03:18:10 +0200

    add javadoc comments to child contract interfaces

--- contracts/interfaces/child/validator/IRewardDistributor.sol
+++ contracts/interfaces/child/validator/IRewardDistributor.sol
@@ -9,11 +9,18 @@ interface RewardToken is IERC20 {
 }
 
 interface IRewardDistributor {
+    /// @notice distributes reward for the given epoch
+    /// @dev transfers funds from sender to this contract
+    /// @param epochId epoch id to distribute rewards for
+    /// @param uptime uptime data for every validator
     function distributeRewardFor(uint256 epochId, Uptime calldata uptime) external;
 
+    /// @notice withdraws pending rewards for the sender (validator)
     function withdrawReward() external;
 
+    /// @notice returns the total reward paid for the given epoch
     function paidRewardPerEpoch(uint256 epochId) external view returns (uint256);
 
+    /// @notice returns the pending reward for the given account
     function pendingRewards(address account) external view returns (uint256);
 }
