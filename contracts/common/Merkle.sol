b6090db536a5041291daaed03abbf9543b5001fe
qedk <1994constant@gmail.com>
2023-06-13 04:34:32 +0400

    chore: add code comments

--- contracts/common/Merkle.sol
+++ contracts/common/Merkle.sol
@@ -1,16 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.19;
 
-import "@openzeppelin/contracts/utils/math/Math.sol";
-
 /**
  * @title Merkle
- * @author Polygon Technology (similar to ENS, but written from scratch)
- * @notice library for checking membership in a merkle tree
+ * @author QEDK
+ * @notice Gas optimized verification of proof of inclusion for a leaf in an ordered Merkle tree
  */
 library Merkle {
-    using Math for uint256;
-
     /**
      * @notice checks membership of a leaf in a merkle tree
      * @param leaf keccak256 hash to check the membership of
@@ -25,6 +21,7 @@ library Merkle {
         bytes32 rootHash,
         bytes32[] calldata proof
     ) internal pure returns (bool isMember) {
+        // solhint-disable-next-line no-inline-assembly
         assembly ("memory-safe") {
             // if proof is empty, check if the leaf is the root
             if proof.length {
@@ -33,21 +30,28 @@ library Merkle {
                 // set iterator to the start of the proof array
                 let i := proof.offset
                 // prettier-ignore
+                // solhint-disable-next-line no-empty-blocks
                 for {} 1 {} {
                     // if index is odd, leaf slot is at 0x20, else 0x0
                     let leafSlot := shl(5, and(0x1, index))
+                    // store the leaf at the calculated slot
                     mstore(leafSlot, leaf)
                     // store proof element in whichever slot is not occupied by the leaf
                     mstore(xor(leafSlot, 32), calldataload(i))
+                    // hash the first 64 bytes in memory
                     leaf := keccak256(0, 64)
+                    // shift index right by 1 bit to divide by 2
                     index := shr(1, index)
+                    // increment iterator by 32 bytes
                     i := add(i, 32)
+                    // break if iterator is at the end of the proof array
                     if iszero(lt(i, end)) {
                         break
                     }
                 }
             }
-            // if index was invalid, or computed root does not match, return false
+            // checks if the calculated root matches the expected root
+            // then, check if index was zeroed while calculating proof, else an invalid index was provided
             isMember := and(eq(leaf, rootHash), iszero(index))
         }
     }
@@ -56,7 +60,7 @@ library Merkle {
      * @notice checks membership of a leaf in a merkle tree with expected height
      * @param leaf keccak256 hash to check the membership of
      * @param index position of the hash in the tree
-     * @param numLeaves number of leaves in the merkle tree (used to calculate the proof length)
+     * @param numLeaves number of leaves in the merkle tree (used to check the proof length)
      * @param rootHash root hash of the merkle tree
      * @param proof an array of hashes needed to prove the membership of the leaf
      * @return isMember a boolean value indicating if the leaf is in the tree or not
@@ -68,6 +72,7 @@ library Merkle {
         bytes32 rootHash,
         bytes32[] calldata proof
     ) internal pure returns (bool isMember) {
+        // solhint-disable-next-line no-inline-assembly
         assembly ("memory-safe") {
             // if proof is empty, check if the leaf is the root
             if proof.length {
@@ -76,21 +81,29 @@ library Merkle {
                 // set iterator to the start of the proof array
                 let i := proof.offset
                 // prettier-ignore
+                // solhint-disable-next-line no-empty-blocks
                 for {} 1 {} {
                     // if index is odd, leaf slot is at 0x20, else 0x0
                     let leafSlot := shl(5, and(0x1, index))
+                    // store the leaf at the calculated slot
                     mstore(leafSlot, leaf)
                     // store proof element in whichever slot is not occupied by the leaf
                     mstore(xor(leafSlot, 32), calldataload(i))
+                    // hash the first 64 bytes in memory
                     leaf := keccak256(0, 64)
+                    // shift index right by 1 bit to divide by 2
                     index := shr(1, index)
+                    // increment iterator by 32 bytes
                     i := add(i, 32)
+                    // break if iterator is at the end of the proof array
                     if iszero(lt(i, end)) {
                         break
                     }
                 }
             }
-            // if index was invalid, or computed root does not match, return false
+            // check if log2(numLeaves) is an integer, or else rounds up to the next integer
+            // then, checks if the calculated root matches the expected root
+            // finally, checks if index was zeroed while calculating proof, else an invalid index was provided
             isMember := and(
                 and(lt(numLeaves, add(shl(proof.length, 0x1), 1)), gt(numLeaves, shl(sub(proof.length, 1), 0x1))),
                 and(eq(leaf, rootHash), iszero(index))
