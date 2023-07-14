1ae028df9ba23bf25c500db86e37aa93fa6b8334
Pano Skylakis <pano.skylakis@immutable.com>
2023-07-14 11:25:41 +1000

    Validator struct

--- contracts/interfaces/IStateSender.sol
+++ contracts/interfaces/IStateSender.sol
@@ -2,8 +2,11 @@
 pragma solidity 0.8.19;
 
 interface IStateSender {
+    // TODO: Move Validator struct from CheckpointManager to common lib
     struct Validator {
-        uint256[4] pubkey;
+        address _address;
+        uint256[4] blsKey;
+        uint256 votingPower;
     }
     
     function syncState(address receiver, bytes calldata data) external;
